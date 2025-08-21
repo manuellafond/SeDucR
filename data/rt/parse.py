d5184a87e9a4e5acde347428d8412c1652f4e0e8
Yao-ban Chan <yaoban@unimelb.edu.au>
2025-08-21 12:37:14 +1000

    rt

--- data/rt/parse.py
+++ data/rt/parse.py
@@ -1,5 +1,19 @@
 import re
 
+counter = 0
+
+
+def countValue():
+    global counter
+    counter += 1
+    return counter
+
+
+def strtr(s, repl):
+    pattern = "|".join(map(re.escape, sorted(repl, key=len, reverse=True)))
+    return re.sub(pattern, lambda m: f"{repl[m.group()]}_{countValue()}", s)
+
+
 file = open("rt_segdup.txt")
 input = file.readline()
 file.close()
@@ -9,12 +23,14 @@ srSpecies = open("rt-sp.txt", "w")
 srSpecies.write(re.sub(r"\w+", r"'\g<0>'", input[4 : input.find('"', 5)] + ";"))
 srSpecies.close()
 
-geneNum = 1
 srGenes = open("rt-genes.txt", "w")
-genesList = re.sub(" -G ", "\n", input[input.find('"', 5) + 1 :]).split("\n")[1:-1]
-for g in genesList:
-    g = g[1 : g.find('"', 1)]
-    g = re.sub(r"\w+", r"\g<0>_" + f"{geneNum}", g) + ";"
-    srGenes.write(g + "\n")
-    geneNum += 1
+genesList = input[input.find('"', 5) + 1 :].split(" -G ")[1:-1]
+for gtree in genesList:
+    maps = gtree[gtree.find('"', 1) + 3 : -1]
+    maps = dict(e.split(":") for e in maps.split(" "))
+
+    gtree = gtree[1 : gtree.find('"', 1)]
+    gtree = strtr(gtree, maps)
+
+    srGenes.write(f"{gtree};\n")
 srGenes.close()
