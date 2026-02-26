8cd3ae04f727b66b5a080a7840f21c3d651cf814
Manuel Lafond <manuellafond@hotmail.com>
2026-02-26 17:04:26 -0500

    make_stats can now compute number of suboptimal instances

--- experiments/make_stats.py
+++ experiments/make_stats.py
@@ -21,6 +21,23 @@ parser.add_argument(
     help="Set kowhai mode to True"
 )
 
+parser.add_argument(
+    "--mode",
+    type=str,
+    default="avg",
+    help="Must be avg or error.  avg mode outputs avg per parameter, error outputs number of subopt instances for each method"
+)
+
+
+parser.add_argument(
+    "--duprate",
+    type=str,
+    default=None,
+    help="In WGD mode, give this to only get error count for specified duprate."
+)
+
+
+
 args = parser.parse_args()
 
 kowhai_mode = args.kowhai
@@ -36,10 +53,15 @@ colnames_in_key = dict()
 
 if kowhai_mode:
     colnames_in_key = { "nh" : "nh", "np" : "np" , "rb" : "rb" , "pc" : "pc", "pj" : "pj" , "d" : "dup_cost" }
+    if args.mode == "error":
+        colnames_in_key["repl"] = "repl"
 else:
     colnames_in_key["WGD"] = "planted_wgds"
     colnames_in_key["duprate"] = "duprate"
     colnames_in_key["d"] = "dup_cost"
+    
+    if args.mode == "error":
+        colnames_in_key["simid"] = "simid"
 
 
 def get_key(vals, colmap):
@@ -78,6 +100,10 @@ with open(stats_file, "r") as f:
         #key = f'WGD={vals[colmap["planted_wgds"]]}, duprate={vals[colmap["duprate"]]}, d={vals[colmap["dup_cost"]]}'
         key = get_key(vals, colmap)
         
+        if args.duprate != None and vals[colmap["duprate"]] != args.duprate:
+            continue
+        
+        
         longest_key = max(longest_key, len(key))
         
         if key not in data:
@@ -92,22 +118,59 @@ with open(stats_file, "r") as f:
         data[key][algo]["solution_cost"].append( float(vals[colmap["solution_cost"]]) )  
         
         
-
-print("Average times (seconds)")
-for key in data:
-    print("\nDataset " + key)
-    
-    for algo in data[key]:
-        mean_time = np.mean(data[key][algo]["time"])
-        print(f"{algo:20} : {mean_time:0.5f}")
+if args.mode == "avg":
+    print("Average times (seconds)")
+    for key in data:
+        print("\nDataset " + key)
         
-print("\n-----------------------------------------")
-print("Average solution costs")
-for key in data:
-    print("\nDataset " + key)
+        for algo in data[key]:
+            mean_time = np.mean(data[key][algo]["time"])
+            print(f"{algo:20} : {mean_time:0.5f}")
+            
+    print("\n-----------------------------------------")
+    print("Average solution costs")
+    for key in data:
+        print("\nDataset " + key)
+        
+        for algo in data[key]:
+            mean_cost = np.mean(data[key][algo]["solution_cost"])
+            print(f"{algo:20} : {mean_cost}")
+elif args.mode == "error":
     
-    for algo in data[key]:
-        mean_cost = np.mean(data[key][algo]["solution_cost"])
-        print(f"{algo:20} : {mean_cost}")
+    err_per_method = dict()
+    instances_per_method = dict()
+    
+    for key in data:
+        for algo in data[key]:
+            
+            if algo not in err_per_method:
+                err_per_method[algo] = 0
+                instances_per_method[algo] = 0
+            
+            instances_per_method[algo] += 1
+            
+            opt = data[key]["insider"]["solution_cost"][0]
+            algocost = data[key][algo]["solution_cost"][0]
+            
+            if len(data[key][algo]["solution_cost"]) > 1:
+                print(f"Warning: entry {key}.{algo} has multiple solution costs, no supposed to happen in mode error")
+            
+            if algocost > opt:
+                err_per_method[algo] += 1
+            elif algocost < opt:
+                print(f"WARNING: better than insider: {key}.{algo}")
+        
+    print("Nb suboptimal solutions per method")
+    print(err_per_method)
+    
+    print("Nb instances per method")
+    print(instances_per_method)
+        
+        
+    
+    
+    
+    
+    
 
 
